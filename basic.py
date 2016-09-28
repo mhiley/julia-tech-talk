bae86b0a23d6e3760c345db974abf88085b0ff31
Michael Hiley <hiley@earthlinginteractive.com>
2016-09-28 18:14:29 -0500

    make basic python array example usable in talk

--- basic.py
+++ basic.py
@@ -1,33 +1,41 @@
 import numpy
 
-# declare arrays w/ a million elements each
-a = numpy.linspace(0.0, 1.0, 1000000)
-b = numpy.linspace(1.0, 2.0, 1000000)
+from util import timeit
 
-# sum elements of array
-total = 0
-for i in range(len(a)):
-    total += a[i]
-
-# add arrays together
-added = []
-for i in range(len(a)):
-    added.append(a[i] + b[i])
+@timeit
+def array_sum_slow(a):
+    total = 0
+    for i in range(len(a)):
+        total += a[i]
+    return total
 
 
-
-# declare arrays w/ a million elements each
-a = numpy.linspace(0.0, 1.0, 1000000)
-b = numpy.linspace(1.0, 2.0, 1000000)
-
-# sum elements of array
-total = a.sum()
-
+@timeit
+def array_add_slow(a, b):
+    added = []
+    for i in range(len(a)):
+        added.append(a[i] + b[i])
+    return added
 
 
-# add arrays together
-added  = a + b
+@timeit
+def array_sum_fast(a):
+    return a.sum()
 
 
+@timeit
+def array_add_fast(a, b):
+    return a + b
 
 
+if __name__ == '__main__':
+
+    # declare arrays w/ a million elements each
+    aa = numpy.linspace(0.0, 1.0, 1000000)
+    bb = numpy.linspace(1.0, 2.0, 1000000)
+
+    array_sum_slow(aa)
+    array_add_slow(aa, bb)
+
+    array_sum_fast(aa)
+    array_add_fast(aa, bb)
