b8f115740ff76720dc3dcc6d2eb6ce5c52330f36
Michael Hiley <hiley@earthlinginteractive.com>
2016-09-28 21:53:07 -0500

    bump amount of work done by pmap example

--- ParallelMapDemo.jl
+++ ParallelMapDemo.jl
@@ -1,7 +1,9 @@
 # Parallel Map (pmap) example
+# Note, initial compile/load times seem to be large here
+#       so don't expect x2 performance from 2 cores.
 
 # a list of four 1000x1000 matrices
-M = Matrix{Float64}[rand(1000,1000) for i=1:4]
+M = Matrix{Float64}[rand(1000,1000) for i=1:10]
 
 # pmap lets us easily call a function in parallel on ten matrices.
 # (svd is singular value decomposition)
