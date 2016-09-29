32eb2b07c8ba4a8c421b1481df5b8077868041a4
Michael Hiley <hiley@earthlinginteractive.com>
2016-09-28 20:51:39 -0500

    didn't actually save file apparently

--- GadflyDemo.jl
+++ GadflyDemo.jl
@@ -1,13 +1,13 @@
 using Gadfly, RDatasets
 
 # simple line plot
-#x = linspace(0., 2.0*pi, 1000)
-#plot(layer(x=x, y=sin(x), Geom.line),
-#     layer(x=x, y=cos(x), Geom.line), Guide.title("waves"))
+x = linspace(0., 2.0*pi, 1000)
+plot(layer(x=x, y=sin(x), Geom.line),
+     layer(x=x, y=cos(x), Geom.line), Guide.title("waves"))
 
 
 # Histogram
-#plot(dataset("car", "SLID"), x="Wages", color="Language", Geom.histogram)
+plot(dataset("car", "SLID"), x="Wages", color="Language", Geom.histogram)
 
 
 # contours and color bar
